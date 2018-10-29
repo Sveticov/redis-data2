de4c14f356d716f8f89841faa644348a382b73ad
Sveticov <sveticovdanil@gmail.com>
2018-10-29 16:30:21 +0300

    <version>1.5.8.RELEASE</version>

--- src/main/java/com/svetikov/redisdata2/RedisData2Application.java
+++ src/main/java/com/svetikov/redisdata2/RedisData2Application.java
@@ -10,20 +10,20 @@ import org.springframework.data.redis.serializer.GenericToStringSerializer;
 @SpringBootApplication
 public class RedisData2Application {
 
-    @Bean
-    public JedisConnectionFactory jedisConnectionFactory() {
-        JedisConnectionFactory jedisConnectionFactory=new JedisConnectionFactory();
-
-        return jedisConnectionFactory;
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        final RedisTemplate<String,  Object> template = new RedisTemplate<String, Object>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
-        return template;
-    }
+//    @Bean
+//    public JedisConnectionFactory jedisConnectionFactory() {
+//        JedisConnectionFactory jedisConnectionFactory=new JedisConnectionFactory();
+//
+//        return jedisConnectionFactory;
+//    }
+//
+//    @Bean
+//    public RedisTemplate<String, Object> redisTemplate() {
+//        final RedisTemplate<String,  Object> template = new RedisTemplate<String, Object>();
+//        template.setConnectionFactory(jedisConnectionFactory());
+//        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+//        return template;
+//    }
 
     public static void main(String[] args) {
         SpringApplication.run(RedisData2Application.class, args);
